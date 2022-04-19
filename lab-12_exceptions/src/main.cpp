d34faa79ccc80d11c4861375416280c4f1cb63b7
MusinaPolina <pmusina03@gmail.com>
2022-04-19 21:49:16 +0300

    lab-12 bad_allocate fix?

--- lab-12_exceptions/src/main.cpp
+++ lab-12_exceptions/src/main.cpp
@@ -7,55 +7,57 @@ size_t reg_to_int(std::string reg) {
 }
 
 int main(int argc, char* argv[]) {
-    try {
-        std::vector <Matrix> regs(10);
-        while (std::cin) {
-            std::string cmd, reg;
-            std::cin >> cmd;
-            if (cmd == "load") {
-                std::string fname;
-                std::cin >> reg >> fname;
-                try {
-                    regs[reg_to_int(reg)] = Matrix::load(fname);
-                } catch (const UnableOpenFileException& e){
-                    std::cout << e.what() << std::endl;
-                } catch (const InvalidFileFormatException& e){
-                    std::cout << e.what() << std::endl;
-                }
-            } else if (cmd == "print") {
-                std::cin >> reg;
-                std::cout << regs[reg_to_int(reg)];
-            } else if (cmd == "add") {
-                std::string oth;
-                std::cin >> reg >> oth;
-                try {
-                    regs[reg_to_int(reg)] += regs[reg_to_int(oth)];
-                } catch (const AddException& e){
-                    std::cout << e.what() << std::endl;
-                }
-            } else if (cmd == "mul") {
-                std::string oth;
-                std::cin >> reg >> oth;
-                try {
-                    regs[reg_to_int(reg)] *= regs[reg_to_int(oth)];
-                } catch (const MulException& e){
-                    std::cout << e.what() << std::endl;
-                }
-            } else if (cmd == "elem") {
-                size_t row, col;
-                std::cin >> reg >> row >> col;
-                try {
-                    std::cout << regs[reg_to_int(reg)][row][col] << std::endl;
-                } catch (const AccessException& e){
-                    std::cout << e.what() << std::endl;
-                }
-            } else {
-                assert(cmd == "exit");
-                break;
+    std::vector <Matrix> regs(10);
+    while (std::cin) {
+        std::string cmd, reg;
+        std::cin >> cmd;
+        if (cmd == "load") {
+            std::string fname;
+            std::cin >> reg >> fname;
+            try {
+                regs[reg_to_int(reg)] = Matrix::load(fname);
+            } catch (const UnableOpenFileException& e){
+                std::cout << e.what() << std::endl;
+            } catch (const InvalidFileFormatException& e){
+                std::cout << e.what() << std::endl;
+            } catch (const std::bad_alloc& e) {
+                std::cout << BadAllocException().what() << std::endl;
             }
+        } else if (cmd == "print") {
+            std::cin >> reg;
+            std::cout << regs[reg_to_int(reg)];
+        } else if (cmd == "add") {
+            std::string oth;
+            std::cin >> reg >> oth;
+            try {
+                regs[reg_to_int(reg)] += regs[reg_to_int(oth)];
+            } catch (const AddException& e){
+                std::cout << e.what() << std::endl;
+            } catch (const std::bad_alloc& e) {
+                std::cout << BadAllocException().what() << std::endl;
+            }
+        } else if (cmd == "mul") {
+            std::string oth;
+            std::cin >> reg >> oth;
+            try {
+                regs[reg_to_int(reg)] *= regs[reg_to_int(oth)];
+            } catch (const MulException& e){
+                std::cout << e.what() << std::endl;
+            } catch (const std::bad_alloc& e) {
+                std::cout << BadAllocException().what() << std::endl;
+            }
+        } else if (cmd == "elem") {
+            size_t row, col;
+            std::cin >> reg >> row >> col;
+            try {
+                std::cout << regs[reg_to_int(reg)][row][col] << std::endl;
+            } catch (const AccessException& e){
+                std::cout << e.what() << std::endl;
+            }
+        } else {
+            assert(cmd == "exit");
+            break;
         }
-    } catch (const std::bad_alloc& e) {
-        std::cout << BadAllocException().what() << std::endl;
     }
     return 0;
 }
